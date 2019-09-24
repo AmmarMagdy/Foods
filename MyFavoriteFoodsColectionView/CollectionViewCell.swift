c9778abe684380280078810bd0642a12583f2006
AmmarMagdy <ammar.m.badawi@gmail.com>
2019-09-24 13:18:59 +0200

    first commit

---
+++ MyFavoriteFoodsColectionView/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MyFavoriteFoodsColectionView
+//
+//  Created by Afnan on 6/25/19.
+//  Copyright © 2019 ammar. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MyFavoriteFoodsColectionView/FoodItems.swift
@@ -0,0 +1,9 @@
+//
+//  FoodItems.swift
+//  MyFavoriteFoodsColectionView
+//
+//  Created by Afnan on 6/25/19.
+//  Copyright © 2019 ammar. All rights reserved.
+//
+
+import Foundation
